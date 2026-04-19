449c46407e9d0270322ecd377b356bdb1f459b92
jason-w-cramer <jcramer85@icloud.com>
2026-04-19 15:49:41 -0600

    Added timer module. I have used and tested this module in other projects already so there is no dedicated test bench for it.

---
+++ src/design/tim.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/19/2026 03:36:41 PM
+// Design Name: 
+// Module Name: tim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tim(
+
+    );
+endmodule
